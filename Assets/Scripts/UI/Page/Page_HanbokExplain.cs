9b9978ba7099340614142a079b09c449d0ef9fd2
zolryeol <116062951+zolryeol@users.noreply.github.com>
2025-07-18 19:52:52 +0900

    [이준형] 프리팹 일부제거 + 한복설명페이지 제작중

---
+++ Assets/Scripts/UI/Page/Page_HanbokExplain.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Page_HanbokExplain : MonoBehaviour
+{
+ 
+    public void InitPage()
+    {
+
+    }
+}
