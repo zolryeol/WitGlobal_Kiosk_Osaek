42253a24efd0186d489616c43c98f197bdb91cf5
zolryeol <116062951+zolryeol@users.noreply.github.com>
2025-09-04 16:05:19 +0900

    [이준형]오색시장 및 교통안내 임시 카테고리 추가/

--- Assets/Scripts/UI/Page/Page_HereInfo.cs
+++ Assets/Scripts/UI/Page/Page_HereInfo.cs
@@ -8,7 +8,7 @@ public class Page_HereInfo : MonoBehaviour
     [SerializeField]
     List<SecondCategoryButton> secondCategoryButtons = new();
     [SerializeField]
-    GameObject[] pages = new GameObject[3];
+    List<GameObject> pages = new();
 
     public void Init()
     {
@@ -18,7 +18,7 @@ public class Page_HereInfo : MonoBehaviour
             int index = i;
             secondCategoryButtons[index].onClick.AddListener(() =>
             {
-                for (int j = 0; j < pages.Length; j++)
+                for (int j = 0; j < pages.Count; j++)
                 {
                     pages[j].SetActive(j == index);
                 }
@@ -44,7 +44,7 @@ public class Page_HereInfo : MonoBehaviour
         // 첫번째 버튼 선택 상태로 설정
         UIManager.Instance.DeselectAllCustomButtons(secondCategoryButtons.ToList());
         secondCategoryButtons[0].SetSelected(true);
-        for (int i = 0; i < pages.Length; i++)
+        for (int i = 0; i < pages.Count; i++)
         {
             pages[i].SetActive(false);
         }
